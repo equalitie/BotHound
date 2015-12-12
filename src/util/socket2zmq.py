34445ce431228e01a786b43b8b316ade80439a9b
Anton Mazhurin <mazhurin@gmail.com>
2015-12-12 11:09:16 -0500

    1. Fix: create zmq socket once, not per connection. 2. Use LineReceiver to implement the basic framing of greymemory messages 3. Adding all the configuration parameters in the command line arguments

--- src/util/socket2zmq.py
+++ src/util/socket2zmq.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from twisted.internet.protocol import Protocol, Factory
+from twisted.protocols import basic
 from twisted.internet import reactor
 import optparse
 import zmq
@@ -10,33 +11,36 @@ from crypto import encrypt
 
 ### Protocol Implementation
 
-class SocketToZmq(Protocol):
-    def __init__(self, zmq_socket,passphrase):
+class SocketToZmq(basic.LineReceiver):
+    delimiter = '\n'
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def lineReceived(self, line):
+        # Ignore blank lines
+        if not line: return
+        """
+        As soon as any line is received, write it into zmq socket.
+        """
+        print line
+        iv, ciphertext, tag = encrypt(self.factory.hashed_key,line, "")
+        self.factory.publisher.send_multipart([self.factory.zmq_tag, iv + ciphertext + tag])
+        self.factory.publisher.send_multipart([self.factory.zmq_tag, line])
+
+class SocketToZmqFactory(Factory):
+    def buildProtocol(self, addr):
+        return SocketToZmq(self);
+
+    def __init__(self, zmq_socket, passphrase, zmq_tag):
         # Prepare our context and publisher
         self.context   = zmq.Context()
         self.publisher = self.context.socket(zmq.PUB)
         self.publisher.bind(zmq_socket)
+        self.zmq_tag = zmq_tag
 
         key = hashlib.sha256(passphrase)
         self.hashed_key = key.digest()
-
-
-    def dataReceived(self, data):
-        """
-        As soon as any data is received, write it into zmq socket.
-        """
-        print data
-        iv, ciphertext, tag = encrypt(self.hashed_key,data, "")
-
-        self.publisher.send_multipart(["greymemory_info", iv + ciphertext + tag])
-
-class SocketToZmqFactory(Factory):
-    def buildProtocol(self, addr):
-        return SocketToZmq(self.zmq_socket,self.passphrase);
-
-    def __init__(self, zmq_socket, passphrase):
-        self.zmq_socket = zmq_socket
-        self.passphrase = passphrase
 
 def main():
     parser = optparse.OptionParser()
@@ -46,16 +50,26 @@ def main():
                       default=22622,
                       help="ZMQ socket port")
 
-    parser.add_option("-P", "--port",
+    parser.add_option("-G", "--greymemoryport",
                       action="store", dest="port",
                       default=22623,
                       help="TCP socket port")
 
+    parser.add_option("-P", "--passphrase",
+                      action="store", dest="passphrase",
+                      default="drawnandquarterly",
+                      help="Passphrase")
+
+    parser.add_option("-T", "--zmqtag",
+                      action="store", dest="zmqtag",
+                      default="greymemory_info",
+                      help="Zmq tag for messages")
+
     (options, args) = parser.parse_args()
 
-    f = SocketToZmqFactory("tcp://*:%s" % options.zmqport, "drawnandquarterly")
-    print "Listening on port %s, publishing on ZMQ port %s ..." % (options.port, options.zmqport)
-    f.protocol = SocketToZmq
+    f = SocketToZmqFactory("tcp://*:%s" % options.zmqport, options.passphrase, options.zmqtag)
+    print "ZMQ relay service."
+    print "Listening on port %s, relaying to ZMQ port %s ..." % (options.port, options.zmqport)
     reactor.listenTCP(options.port, f)
     reactor.run()
 
