5145a363dbee72dcd76c6daa3fc08a19cb534346
Veggie Monster <vmonmoonshine@gmail.com>
2016-02-03 17:47:25 -0500

    copied es_log_muncher from l2b

---
+++ src/util/es_log_muncher.py
@@ -0,0 +1,66 @@
+"""
+Simply getting the relevant fields from es data and put them into
+ats record
+"""
+from os.path import dirname
+
+UNREASANABLE_SIZE = 100*1024*1024 #100MB
+NORMAL_HTTP_METHODS = ["GET", "HEAD", "POST"]
+VALID_HTTP_METHODS = ["OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT", "PROPFIND", "PROPPATCH", "MKCOL", "COPY", "MOVE", "LOCK", "UNLOCK", "VERSION-CONTROL", "REPORT", "CHECKOUT", "CHECKIN", "UNCHECKOUT", "MKWORKSPACE", "UPDATE", "LABEL", "MERGE", "BASELINE-CONTROL", "MKACTIVITY", "ORDERPATCH", "ACL"]
+
+def parse_es_json_object(hit_json_object):
+    res = hit_json_object["_source"]
+    ats_res = {}
+    ats_res["host"] = res["client_ip"]
+    if res["client_ua"] == "-":
+        ats_res["agent"] = None
+    else:
+        ats_res["agent"] = res["client_ua"]
+
+    ats_res["status"] = int(res["http_response_code"])
+
+    ats_res["http_ver"] = res["http_request_version"]
+    ats_res["method"] = res["client_request_method"]
+    ats_res["request"] = res["client_url"]
+
+    if ats_res["method"] not in VALID_HTTP_METHODS:
+        print "ignoring request with invalid method %s in %s"%(ats_res["method"],line)
+        return None
+    elif ats_res["method"] not in NORMAL_HTTP_METHODS:
+        print "abnormal http request, somebody messing around?:", line
+
+    #the specific extension is only important in text/html otherwise
+    #the general type gives us enough information
+    doc_type = str(res["content_type"]).split('/')
+    if len(doc_type) == 2:
+        #check for non standard
+        ats_res["type"] = str(doc_type[1]) if str(doc_type[1]) == 'html' else str(doc_type[0])
+    else:
+        #the doc type is not valid perhapse because the request has not been served
+        #we guess the type from the request then
+        ats_res["type"] = ats_res["request"].find('.', ats_res["request"].rfind('/')) == -1 and 'html' or ats_res["request"].split('.')[-1]
+
+        #group all the images
+        ats_res["type"] = res["type"] in ["jpg", "gif", "png"] and "image" or res["content_type"]
+
+    #if (not res["type"] == 'html'): print res["type"], line
+
+    if res["reply_length_bytes"] == "-":
+        ats_res["size"] = 0
+    else:
+        ats_res["size"] = int(res["reply_length_bytes"])
+
+    #just to observe the illogically large sizes
+    if (ats_res["size"] >= UNREASANABLE_SIZE):
+        print "unreasonably large payload ", line
+        return None
+
+    timestamp = res["@timestamp"]
+    ats_res["time"] = timestamp[:timestamp.find('.000Z')] + "Z"
+
+    return ats_res;
+
+if __name__ == "__main__":
+    for files in glob.glob("*.log"):
+        for l in open(files,'rb'):
+	    parse_line(l)
